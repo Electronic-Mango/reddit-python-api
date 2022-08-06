d81ea75021bf5f1cf6ccf9e393d99f8461588729
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2022-08-06 14:34:53 +0200

    Add endpoint returning random submission for a given subreddit

--- src/api.py
+++ src/api.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from markupsafe import escape
+
+from reddit import get_random_submission
 
 app = Flask(__name__)
 
@@ -6,3 +9,17 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "<p>Hello!</p>"
+
+
+@app.route("/submission/<subreddit_name>")
+def random_submission(subreddit_name: str):
+    escaped_subreddit_name = escape(subreddit_name)
+    submission = get_random_submission(escaped_subreddit_name)
+    return {
+        "id": submission.id,
+        "url": submission.url,
+        "title": submission.title,
+        "author": submission.author.name,
+        "nsfw": submission.over_18,
+        "spoiler": submission.spoiler,
+    }
