9992edf497a4f9f1ef7d513e6e0826be85b02608
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2023-11-13 13:51:00 +0100

    Use README.md as PyPi description

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 from distutils.core import setup
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name="reddit-python-api",
     packages=["redditpythonapi"],
     version="0.2",
     license="GPLv3",
     description="A simple Reddit Python API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Electronic Mango",
     author_email="78230210+Electronic-Mango@users.noreply.github.com",
     url="https://github.com/Electronic-Mango/reddit-python-api",
