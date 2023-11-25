73a31606482619349c64d130423f9a0cefe2a094
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2023-11-25 21:53:40 +0100

    Add more paths in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,11 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-import redditpythonapi
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.abspath("../redditpythonapi"))
 
 project = "Reddit Python API"
 copyright = "2023, Electronic-Mango"
