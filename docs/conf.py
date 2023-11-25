82b9e1ba94e47586a7810e0d6729016e166554fc
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2023-11-25 21:46:23 +0100

    Fix included paths in Sphinx docs

--- docs/conf.py
+++ docs/conf.py
@@ -6,6 +6,11 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(".."))
+
 project = "Reddit Python API"
 copyright = "2023, Electronic-Mango"
 author = "Electronic-Mango"
